130ab29401dbb1703880105b90ee8bb6155f649f
Hovo <avanyanhovhannes365@gmail.com>
2020-08-20 16:07:35 +0400

    remove user class add bookTicket, showMyTickets functions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # movie_tickets_booking
+A simple c++ mini project which i found in web and wanted to implement for learning purposes.
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include "user.h"
 #include <bits/stdc++.h> 
 #include <fstream>
 #include <sstream>
@@ -9,32 +8,42 @@
 
 using namespace std;
 
+struct info {
+    vector <string> movie_id;
+    vector <string> seat;
+};
+
+
 // helper functions
 string login_reg(); //make login or registration
-vector<string> splitSentence(string);
+vector<string> splitSentence(string, char a = ' ');
 int getIndex(vector<string> v, string K);
+info getInfo(string);
 //
 
+// main functions // 
+
 void showMenu();
-void bookticket();
-void showMyTickets(int);
-void addNewMovie();
-void deleteMovie();
+void bookTicket(string, string, string);
+void showMyTickets();
 void showMovieList();
 void exitOrMenu();
+// 
+
+string login;
 
 int main() {
     
-    User user(login_reg()); // return login and create user object with that login
+    login = login_reg(); // return login and create user object with that login
     showMenu();
-
+    
     return 0;
 }
 
 
-/// functions ///
-/// functions ///
-/// functions ///
+/// main functions ///
+/// main functions ///
+/// main functions ///
 
 void showMenu() {
     string menu = R"(Welcome to Movie Ticket Booking System
@@ -42,10 +51,8 @@ void showMenu() {
 Menu
 1. Book tiket
 2. Show my tickets
-3. Add new movie
-4. Delete movie
-5. Show movie list
-6. Exit
+3. Show movie list
+4. Exit
 Choose one: )";
     cout << menu << " ";
     int option;
@@ -53,69 +60,118 @@ Choose one: )";
         cin >> option; 
         switch (option) {
             case 1:
-                system("clear"); // for windows use system("cls")
-                break;
+                {
+                    system("clear"); // for windows use system("cls")
+                    showMovieList();
+                    string movie_id, seat;
+                    cout << "Enter the id of movie which you want to book : ";
+                    cin >> movie_id;
+                    cout << "Where would you like to seat: ";
+                    cin >> seat;
+                    bookTicket(login, movie_id, seat);
+                    cout << "Congratulations uou sucsesfully booked a ticket" << endl;
+                    exitOrMenu();
+                    break;
+                }
             case 2:
-
+                system("clear");
+                showMyTickets();
+                cout << endl;
+                exitOrMenu();
                 break;
+            
             case 3:
-            
-                break;
-            case 4:
-            
-                break;
-            case 5:
                 {
                     showMovieList();
                     exitOrMenu();
                     break;
                 }
-            case 6:
+            case 4:
                 exit(0);
                 break;
 
             default:
-                cout << "You must enter integer between 1 and 7: ";
+                cout << "You should enter a number between 1 and 7: ";
                 break;
             }
-        } while (option > 7 || option < 1);
+        } while (option > 4 || option < 1);
 }
 
-void bookTicket() {
-    // TO DO
+void bookTicket(string login, string movie_id, string seat) {
+    vector <string> data;
+    vector <string> resV;
+    ifstream r("users.txt");
+    string line;
+    while(r.good()) {
+        getline(r, line);
+        data = splitSentence(line);
+        if (data[0] == login) {
+            if(data.size() > 2 ) {
+                data[2] += movie_id + "#";
+                data[3] += seat + "#";
+            }
+            else {
+                data.push_back(movie_id + "#");
+                data.push_back(seat + "#");
+            }
+            string res = data[0] + " " + data[1] + " " + data[2] + " " + data[3];
+            resV.push_back(res);
+            
+        }
+        else {
+            resV.push_back(line);
+        }
+    }
+    
+    
+    ofstream w("users.txt", ios::out);
+    int count = 0;
+    for (string s : resV) {
+        count++;
+        w << s;
+        if (count < resV.size()) w << endl;  
+        
+    }
 }
 
-void showMyTickets(int id) {
+void showMyTickets() {
     // TO DO
-    ifstream moviesFile;
-    moviesFile.open("test.csv");
-    int count = 1;
-    // while (moviesFile.good()) {
-    //     string line;
-    //     getline(moviesFile, line, ',');
-    //     cout << line << " ";
-    //     if (count % 3 == 1 && stoi(line) == id) {
-    //         string row;
-    //         getline(moviesFile, row);
-    //         cout << row;
-    //     }
-    //     count++;
-    // }
-    moviesFile.close();
-}
+    info data = getInfo(login);
+    if (data.movie_id.empty()) {
+        cout << "You didn't booked any ticket yet" << endl;
+        return;
+    }
+    string line;
+    ifstream f("movies.csv");
+    bool flag = false;
+    int count = 0;
+    while (f.good()){
+        getline(f, line);
+        //cout << line << endl;
+        vector <string> tokens = splitSentence(line, ',');
+       // cout << tokens[0];
+        for (string s : data.movie_id) {
+            //cout << s << " " <<  tokens[0] << endl;
+            if (s == tokens[0] || (tokens[0] == "ID" && !flag))
+            {   
+                for (string c : tokens) {
+                    cout << c << " | ";
+                }
+                if (!flag) cout << "Your place |";
+                else {cout << data.seat[count++] << " | ";}
+                flag = true;
+                cout << endl; 
+            }
+            
+        }
+    }
 
-void addNewMovie() {
-    // TO DO
-}
-
-void deletMovie() {
-    // TO DO
 }
 
 void showMovieList() {
     system("clear");
     ifstream moviesFile;
-    moviesFile.open("test.csv");
+    moviesFile.open("movies.csv");
     while (moviesFile.good()) {
         string line;
         getline(moviesFile, line, ',');
@@ -125,10 +181,10 @@ void showMovieList() {
 }
 
 void exitOrMenu() {
-    char op;
+    string op;
     cout << "Press b to go back or anything else to close the program : ";
     cin >> op;
-    if (op == 'b') showMenu();
+    if (op == "b") showMenu();
     else{exit(0);}
 }
 
@@ -136,7 +192,7 @@ void exitOrMenu() {
 /// helper functions ///
 /// helper functions ///
 
-vector<string> splitSentence(string line) {
+vector<string> splitSentence(string line, char a) {
     vector <string> tokens; 
       
     // stringstream class check1 
@@ -144,9 +200,11 @@ vector<string> splitSentence(string line) {
       
     string intermediate; 
       
-    // Tokenizing w.r.t. space ' ' 
-    while(getline(check1, intermediate, ' ')) 
-    { 
+    // Tokenizing w.r.t.
+    int count = 0;
+    while(getline(check1, intermediate, a)) 
+    {   
+        
         tokens.push_back(intermediate); 
     } 
     return tokens;
@@ -203,13 +261,16 @@ string login_reg() {
             string password;
             ifstream userF;
             userF.open("users.txt");
-            vector <string> logins;
+            if (userF.peek() == std::ifstream::traits_type::eof()) {
+                lop = -1;
+            }
+            else {
+                vector <string> logins;
             vector <string> pass;
             int count = 0;
             while (userF.good()) { 
                 string line;
                 getline(userF, line);
-                
                 vector<string> results = splitSentence(line);
                 logins.push_back(results[0]);
                 pass.push_back(results[1]);
@@ -235,6 +296,8 @@ string login_reg() {
                 }
                 if (flag) break;
             }     
+            }
+            
         }
         
         else {
@@ -247,4 +310,36 @@ string login_reg() {
     system("clear");
 
     return login;
+}
+
+info getInfo(string login) {
+    ifstream r("users.txt");
+    info data;
+    while(r.good()) {
+        string line;
+        getline(r, line);
+        vector <string> tokens = splitSentence(line);
+        if(tokens[0] == login) {
+            if (tokens.size() <= 2) return data;
+            int pos = 0;
+            for (int i = 0; i < tokens[2].length(); i++) {
+                if (tokens[2][i] == '#') {
+                    string res = "";
+                    for (int j = pos; j < i; j++) res += tokens[2][j];
+                    data.movie_id.push_back(res);
+                    pos = i + 1;
+                }
+            }
+            pos = 0;
+            for (int i = 0; i < tokens[3].length(); i++) {
+                if (tokens[3][i] == '#') {
+                    string res = "";
+                    for (int j = pos; j < i; j++) res += tokens[3][j];
+                    data.seat.push_back(res);
+                    pos = i + 1;
+                }
+            }
+        }
+    }
+    return data;
 }
